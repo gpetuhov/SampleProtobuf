110955ab08936232e62b416a55932053dfc427da
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-07 13:56:46 +0300

    Save serialized person into file

--- app/src/main/java/com/gpetuhov/android/sampleprotobuf/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampleprotobuf/MainActivity.kt
@@ -2,6 +2,10 @@ package com.gpetuhov.android.sampleprotobuf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pawegio.kandroid.toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,10 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val person = PersonProtoClass.Person.newBuilder()
-//            .setName("Uncle Bob")
-//            .setAge(123)
-//            .setEmail("test@test.com")
-//            .build()
+        savePerson.setOnClickListener { savePerson() }
+    }
+
+    private fun savePerson() {
+        val name = personName.text.toString()
+        val age = personAge.text.toString().toIntOrNull() ?: 0
+        val email = personEmail.text.toString()
+
+        val person = PersonProtoClass.Person.newBuilder()
+            .setName(name)
+            .setAge(age)
+            .setEmail(email)
+            .build()
+
+        try {
+            val path = filesDir.path
+            val file = File("$path/person.txt")
+            val output = FileOutputStream(file)
+            person.writeTo(output)
+
+            toast("Person saved to file")
+
+        } catch (e: Exception) {
+            toast("Error saving person to file")
+        }
     }
 }
